2597cdc66e18daf7bc8e7269daac40ad735f5890
Amit Jha <amitjha@Amits-Mac-mini.attlocal.net>
2020-04-24 21:51:35 -0500

    chapter3 page36

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
+
 @app.route('/')
 def index():
-    print(request)
+    #print(request)
     return request.headers.get('User-Agent')
 
 
 @app.route('/<name>/<id>')
 def user(name,id=0):
-    return '<h1>Hello World in a bug way {}</h1>'.format(id)
+    #print(app.url_map)
+    return render_template('user.html', name = name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 
